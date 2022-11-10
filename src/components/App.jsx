e2fe7df3c138b02e896badbf0887d3d13f1ece3f
EugeneDemchenko <108480374+EugeneDemchenko@users.noreply.github.com>
2022-11-10 02:02:12 +0200

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,41 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
 import Statistics from "./Statistics/Statistics";
 import Section from "./Section/Section";
 import './App.css'
 
-
-class App extends Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-  }
-
-  countFeedback = reaction => {
-    this.setState (prevState =>({[reaction]: prevState[reaction] + 1}))
-  }
+export default function App() {
+  const [good, setGood] = useState( 0 )
+  const [neutral, setNeutral] = useState( 0 )
+  const [bad, setBad] = useState(0)
   
-  render() {
-    const reaction = Object.keys(this.state)
-    const { good, neutral, bad } = this.state;
-    const countTotalFeedback = good + neutral + bad;
-    const countPositiveFeedbackPercentage = Math.round(good /  countTotalFeedback * 100)
-        
-    return (
+  const countFeedback = reaction => {
+    switch (reaction) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
+  }
+
+  const countTotalFeedback = good + neutral + bad;
+  const countPositiveFeedbackPercentage = Math.round(good /  countTotalFeedback * 100)
+
+
+  return (
       <>
       <div className="section">
         <Section title="Please leave feedback" >
           <FeedbackOptions
-            options={reaction}
-            onLeaveFeedback={this.countFeedback}
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={countFeedback}
           />
         </Section>    
         <Section title="Statistics" >
@@ -43,7 +50,4 @@ class App extends Component {
       </div>
       </>
     );
-  }
-}
-
-export default App
+ }
